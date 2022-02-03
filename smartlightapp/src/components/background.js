06a4939ffa7a8dfb8ab9de710d2371d5741105d2
ChristofferRTS <4906240@RTS-365.dk>
2022-02-03 13:24:42 +0100

    make background absolute

--- smartlightapp/src/components/background.js
+++ smartlightapp/src/components/background.js
@@ -1,6 +1,6 @@
 const Bg = () => {
     return ( 
-        <div className="container h-full" style={{backgroundColor:"#0A4DA2"}}>
+        <div className="container absolute h-full -z-10 " style={{backgroundColor:"#0A4DA2"}}>
             <div id="circleContainer" className="w-full h-1/2">
                 <div id="circle" className="absolute w-48 h-48 rounded-full -top-20 -left-14" style={{backgroundColor:"#FFFFFF", opacity:"4.3%"}}/>
                 <div id="circle" className="absolute w-48 h-48 rounded-full top-40" style={{backgroundColor:"#FFFFFF", opacity:"4.3%"}}/>
