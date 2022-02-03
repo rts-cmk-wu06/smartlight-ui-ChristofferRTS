01c2519df17aebc829a45604d59d061f0feace52
ChristofferRTS <4906240@RTS-365.dk>
2022-02-03 13:21:09 +0100

    update colors

--- smartlightapp/src/components/background.js
+++ smartlightapp/src/components/background.js
@@ -1,10 +1,10 @@
 const Bg = () => {
     return ( 
-        <div className="container h-full bg-blue-900">
-            <div id="circleContainer" className="container h-">
-                <div id="circle" className="absolute w-48 h-48 rounded-full opacity-20 bg-sky-800 -top-20 -left-14"/>
-                <div id="circle" className="absolute w-48 h-48 rounded-full bg-sky-800 opacity-20 top-40"/>
-                <div id="circle" className="absolute w-48 h-48 rounded-full bg-sky-800 opacity-20 top-16 -right-20"/>
+        <div className="container h-full" style={{backgroundColor:"#0A4DA2"}}>
+            <div id="circleContainer" className="w-full h-1/2">
+                <div id="circle" className="absolute w-48 h-48 rounded-full -top-20 -left-14" style={{backgroundColor:"#FFFFFF", opacity:"4.3%"}}/>
+                <div id="circle" className="absolute w-48 h-48 rounded-full top-40" style={{backgroundColor:"#FFFFFF", opacity:"4.3%"}}/>
+                <div id="circle" className="absolute w-48 h-48 rounded-full top-16 -right-20" style={{backgroundColor:"#FFFFFF", opacity:"4.3%"}}/>
             </div>
         </div>
      );
