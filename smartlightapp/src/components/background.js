3e890deca41745dacb21c209583f9cd08c57e843
ChristofferRTS <4906240@RTS-365.dk>
2022-02-04 13:49:56 +0100

    fix bgcircle placement

--- smartlightapp/src/components/background.js
+++ smartlightapp/src/components/background.js
@@ -1,10 +1,10 @@
 const Bg = () => {
     return ( 
-        <div className="container absolute h-full -z-10 " style={{backgroundColor:"#0A4DA2"}}>
+        <div id="background" className="container absolute h-full -z-10 " style={{backgroundColor:"#0A4DA2"}}>
             <div id="circleContainer" className="w-full h-1/2">
-                <div id="circle" className="absolute w-48 h-48 rounded-full -top-20 -left-14" style={{backgroundColor:"#FFFFFF", opacity:"4.3%"}}/>
-                <div id="circle" className="absolute w-48 h-48 rounded-full top-40" style={{backgroundColor:"#FFFFFF", opacity:"4.3%"}}/>
-                <div id="circle" className="absolute w-48 h-48 rounded-full top-16 -right-20" style={{backgroundColor:"#FFFFFF", opacity:"4.3%"}}/>
+                <div id="circle_top_left" className="absolute w-48 h-48 rounded-full -top-24 -left-14" style={{backgroundColor:"#FFFFFF", opacity:"4.3%"}}/>
+                <div id="circle_bottom_left" className="absolute w-48 h-48 rounded-full top-36" style={{backgroundColor:"#FFFFFF", opacity:"4.3%"}}/>
+                <div id="circle_right" className="absolute w-48 h-48 rounded-full top-16 -right-20" style={{backgroundColor:"#FFFFFF", opacity:"4.3%"}}/>
             </div>
         </div>
      );
