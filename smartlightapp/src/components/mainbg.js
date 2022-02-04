fec39715142e3ac9a5631ee4e3ee92ab1cf0a48c
ChristofferRTS <4906240@RTS-365.dk>
2022-02-04 13:49:44 +0100

    add mainbg

---
+++ smartlightapp/src/components/mainbg.js
@@ -0,0 +1,9 @@
+
+const MainBg = () => {
+    return ( 
+            <div className="rounded-t-3xl cotainer h-4/5" style={{backgroundColor:"#F6F8FB"}}>
+            </div>
+     );
+}
+ 
+export default MainBg;
