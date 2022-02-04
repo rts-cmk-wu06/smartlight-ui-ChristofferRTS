27db62d53627181188c7c6529d6130adaabd525b
ChristofferRTS <4906240@RTS-365.dk>
2022-02-04 13:50:08 +0100

    add header with content

---
+++ smartlightapp/src/components/header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    return ( 
+        <div className="container flex items-center justify-between px-6 text-white h-1/5">
+            <div>
+                <p className="inline text-3xl font-bold">Control</p>
+                <p className="inline text-3xl">Panel</p>
+            </div>
+            <div className="relative z-0 overflow-visible border-2 rounded-full h-14 w-14 top-3">
+                <div className="relative left-10 z-10 h-2.5 rounded-full w-2.5" style={{backgroundColor:"#FF9898"}}></div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Header;
