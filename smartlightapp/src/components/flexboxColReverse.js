55962db4c1bb0d99be6d02796ab5db243994f72d
ChristofferRTS <4906240@RTS-365.dk>
2022-02-15 16:35:31 +0100

    move root folder

--- smartlightapp/src/components/flexboxColReverse.js
+++
@@ -1,9 +0,0 @@
-const Flex = (props) => {
-    return ( 
-        <div className="flex flex-col-reverse justify-between h-full">
-            {props.children}
-        </div>
-     );
-}
- 
-export default Flex;
